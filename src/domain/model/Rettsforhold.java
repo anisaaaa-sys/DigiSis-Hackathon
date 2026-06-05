570ac4f7ba840d51ffb2b7f3fe6608e87ba0bf79
anisai <anisai@uio.no>
2026-06-05 12:57:43 +0200

    Add parental rights enum

---
+++ src/domain/model/Rettsforhold.java
@@ -0,0 +1,26 @@
+package no.digisis.hackathon.spor3.domain.model;
+
+public enum Rettsforhold {
+    BEGGE("begge");
+
+    KUN_MOR("kun-mor");
+
+    KUN_FAR("kun-far");
+
+    private final String kode;
+
+    Rettsforhold(String kode) {
+        this.kode = kode;
+    }
+
+    public String getKode() {
+        return kode;
+    }
+
+    public static Rettsforhold fraKode(String kode) {
+        for (Rettsforhold r : values()) {
+            if (r.kode.equalsIgnoreCase(kode)) return r;
+        }
+        throw new IllegalArgumentException("Ukjent rettsforhold: " + kode);
+    }
+}
